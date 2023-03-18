857c1216858a3aa5055c97d3e16816bb5bde698b
Elijah Soladoye <shodown96@gmail.com>
2023-03-18 03:58:09 +0100

    fix

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -5,5 +5,5 @@ cd /var/app/staging
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createfirstsuperuser
+python manage.py createsu
 python manage.py collectstatic --noinput
