5e30882ed1ce73e8d06e3a063882f8dabd4cbcd0
Elijah Soladoye <shodown96@gmail.com>
2023-08-19 17:27:53 +0100

    prod

--- lmuvote/wsgi.py
+++ lmuvote/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lmuvote.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lmuvote.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 
